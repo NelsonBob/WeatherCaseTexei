b830719cb434ac322c35a367e27133dc07445a14
unknown <nels.fomekong@gmail.com>
2026-03-12 19:18:42 +0100

    weather from account page

--- force-app/main/default/lwc/weatherComponent/weatherComponent.js
+++ force-app/main/default/lwc/weatherComponent/weatherComponent.js
@@ -5,27 +5,62 @@
 
 
 import { LightningElement, track, api, wire } from 'lwc';
+import { getRecord } from 'lightning/uiRecordApi';
 import getWeatherbyCoordonate from '@salesforce/apex/WeatherController.getWeatherbyCoordonate';
-
+import getWeatherbyCity from '@salesforce/apex/WeatherController.getWeatherbyCity';
 
 const ACCOUNT_FIELDS = ['Account.BillingCity'];
 export default class WeatherComponent extends LightningElement {
-    @api weatherData;
+  
+    
+    @api recordId;
+    @track weatherData;
     @track errorMessage;
 
     city;
     refreshInterval;
 
+    // get city from account
+    @wire(getRecord, { recordId: '$recordId', fields: ACCOUNT_FIELDS })
+    wiredAccount({ error, data }) {
+        if (data) {
 
+            this.city = data.fields.BillingCity.value;
+
+            if(this.city){
+
+                this.fetchWeatherByCity();
+            
+                
+            } else {
+
+                this.weatherData = null;
+                this.errorMessage = "Unable to retrieve city information from the account.";
+            }
+        } 
+        
+        if (error) {
+            
+            this.errorMessage =  error.body.message;
+        }
+     
+
+    }    
     connectedCallback() {
 
       
+            
+        // get weather Home Location
+            if (!this.recordId) {
 
-            this.getLocationWeather();
+                 this.getLocationWeather();
 
-            // refresh toutes les heures
-            this.refreshInterval =
-            setInterval(() => this.getLocationWeather(), 3600000);
+                 // Set up auto-refresh every hour (3600000 milliseconds)
+                 this.refreshInterval =
+                        setInterval(() => this.getLocationWeather(), 3600000);
+            
+            }
+            
 
 
     }
@@ -38,54 +73,71 @@ export default class WeatherComponent extends LightningElement {
 
     }
 
+    fetchWeatherByCity() {
 
+             getWeatherbyCity({ cityName: this.city })
 
+            .then(result => {
 
-        getLocationWeather() {
+                this.weatherData = this.formatWeather(result);
+                this.errorMessage = null;
 
-            navigator.geolocation.getCurrentPosition(
-                (position) => {
+             })
 
-                    getWeatherbyCoordonate({
+            .catch(error => {
 
-                        latitude: position.coords.latitude,
-                        longitude: position.coords.longitude
-                    })
-                    .then ( weatherData => {
-                        this.weatherData = this.formatWeather(weatherData);
-                         this.errorMessage = null;
-                     
-                    })
-                    .catch((error) => {
+                this.weatherData = null;
+                this.errorMessage = error.body.message;
 
-                        this.weatherData = null;
-                        this.errorMessage = error.message;
-                    })
-                    
-                    ;
-                },
+            });
 
-
-            );     
-        
         }
+
+
+    getLocationWeather() {
+
+        navigator.geolocation.getCurrentPosition(
+            (position) => {
+
+                getWeatherbyCoordonate({
+
+                    latitude: position.coords.latitude,
+                    longitude: position.coords.longitude
+                })
+                .then ( weatherData => {
+                    this.weatherData = this.formatWeather(weatherData);
+                    this.errorMessage = null;
+                     
+                })
+                .catch((error) => {
+
+                      this.weatherData = null;
+                      this.errorMessage = error.message;
+                    ;
+                });
+                    
+            },
+
+
+        );     
+        
+    }
                 
             
         
             
         
 
-        formatWeather(obs) { 
-            const condition = obs.clouds || '';
-            return {
-                location: obs.stationName || this.city || "Unknown",
-                
-            temperature: obs.temperature,
-            windSpeed: obs.windSpeed,
-            humidity: obs.humidity,
-            clouds: condition
-            };
-         }
+    formatWeather(obs) { 
+        const condition = obs.clouds || '';
+         return {
+         location: obs.stationName ||  "Unknown",    
+         temperature: obs.temperature,
+         windSpeed: obs.windSpeed,
+         humidity: obs.humidity,
+         clouds: condition
+        };
+    }
 
     
 }
